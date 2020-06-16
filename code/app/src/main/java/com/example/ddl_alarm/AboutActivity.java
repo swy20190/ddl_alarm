f1d7cdaaa2da147b6eccf3c312f9ac887d44b667
swy20190 <K201410109@163.com>
2020-06-16 21:55:00 +0800

    恰饭界面

--- code/app/src/main/java/com/example/ddl_alarm/AboutActivity.java
+++ code/app/src/main/java/com/example/ddl_alarm/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.example.ddl_alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.about_toolbar);
+        toolbar.setTitle("关于作者");
     }
 }
